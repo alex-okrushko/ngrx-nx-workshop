40676699ea445c89199de8a592e3ea8169f65bc4
Alex Okrushko <alex@ts.dev>
2022-10-03 21:42:28 -0400

    m3: Effects and error handing

---
+++ apps/ngrx-workshop/src/app/error.effects.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@angular/core';
+import { Actions, createEffect, ofType } from '@ngrx/effects';
+import { tap } from 'rxjs/operators';
+
+import { MatSnackBar } from '@angular/material/snack-bar';
+import * as productApiActions from './product/actions';
+
+@Injectable()
+export class ErrorEffects {
+  constructor(
+    private readonly actions$: Actions,
+    private readonly snackBar: MatSnackBar
+  ) {}
+
+  handleFetchError$ = createEffect(
+    () => {
+      return this.actions$.pipe(
+        ofType(productApiActions.productsFetchedError),
+        tap(({ errorMessage }) => {
+          this.snackBar.open(errorMessage, 'Error', {
+            duration: 2500,
+          });
+        })
+      );
+    },
+    { dispatch: false }
+  );
+}
